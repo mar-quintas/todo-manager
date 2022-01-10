39933c0f144d6fd8cc5b434584b7ed51c993440a
Martina Quintas <martinaquintas.w@gmail.com>
2022-01-10 10:25:00 -0300

    objects model

--- api-todo-manager/app/todos/models.py
+++ api-todo-manager/app/todos/models.py
@@ -1,1 +1,40 @@
 from app.db import db, BaseModelMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    todo = db.relationship('Todo', backref='user', lazy='dynamic')
+    folder = db.relationship('Folder', backref='user', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140))
+    date = db.Column(db.Date())
+    time = db.Column(db.Time())
+    folder = db.Column(db.String, db.ForeignKey('folder.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<ToDo  {}>'.format(self.title)
+
+class Folder(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    todo = db.relationship('Todo', backref='folder', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Folder {}>'.format(self.name)
