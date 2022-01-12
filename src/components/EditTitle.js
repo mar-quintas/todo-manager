7f8c07ce81319d826586e33e4d63b9d3728d5810
Martina Quintas <martinaquintas.w@gmail.com>
2022-01-12 01:20:01 -0300

    edit view ok

--- src/components/EditTitle.js
+++ src/components/EditTitle.js
@@ -8,19 +8,36 @@ import axios from "axios";
 
 
 function EditTitle (props) {
+  let todo = props.todo
+  const [getTitle, setTitle] = useState(todo.title)
+  const [editMode, setEditMode] = useState(false)
 
-  const [getTitle, setTitle] = useState(props.title)
 
   return(
-    <div id={props.id}>
-     <Form>
-       <Row>
-         <Col lg={true}><Form.Control value={getTitle} type="text" onChange={e => setTitle(e.target.value)}/></Col>
-         <Col md="auto"><Button onClick={()=>{props.editTitle(props.id, getTitle, props.ready); props.setEditMode(false)}} variant="primary" name="save">Save</Button></Col>
-         <Col md="auto"><Button onClick={()=>props.setEditMode(false)} variant="primary" name="delete">Cancel</Button></Col>
-       </Row>
-     </Form>
-    </div>
+    <>
+    {!editMode ?
+      <div id={todo.id}>
+       <Form>
+         <Row>
+           <Col md="auto"><Form.Check aria-label="option 1" defaultChecked={todo.ready} onChange={()=>props.editData(todo.id, todo.title, todo.ready)} /></Col>
+           <Col lg={true}><Form.Control type="text" value={todo.title} disabled/></Col>
+           <Col md="auto"><Button variant="primary" onClick={()=>setEditMode(true)} name="edit">Edit</Button></Col>
+           <Col md="auto"><Button onClick={()=>{props.deleteData(todo.id)}} variant="primary" name="delete">Delete</Button></Col>
+         </Row>
+       </Form>
+      </div>
+      :(
+      <div id={todo.id}>
+       <Form>
+         <Row>
+           <Col lg={true}><Form.Control value={getTitle} type="text" onChange={e => setTitle(e.target.value)}/></Col>
+           <Col md="auto"><Button onClick={()=>{props.editTitle(todo.id, getTitle, todo.ready); setEditMode(false)}} variant="primary" name="save">Save</Button></Col>
+           <Col md="auto"><Button onClick={()=>setEditMode(false)} variant="primary" name="delete">Cancel</Button></Col>
+         </Row>
+       </Form>
+      </div>
+    )}
+    </>
   );
 }
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -11,28 +11,10 @@ function TodoList (props){
 
   useEffect(() => { props.getData() },[])
 
-  const [editMode, setEditMode] = useState(false)
-
   return (
     <>
       { props.todosData.map((todo) => (
-         !editMode ?
-           <div id={todo.id}>
-            <Form>
-              <Row>
-                <Col md="auto"><Form.Check aria-label="option 1" defaultChecked={todo.ready} onChange={()=>props.editData(todo.id, todo.title, todo.ready)} /></Col>
-                <Col lg={true}><Form.Control type="text" value={todo.title} disabled/></Col>
-                <Col md="auto"><Button variant="primary" onClick={()=>setEditMode(true)} name="edit">Edit</Button></Col>
-                <Col md="auto"><Button onClick={()=>{props.deleteData(todo.id)}} variant="primary" name="delete">Delete</Button></Col>
-              </Row>
-            </Form>
-           </div>
-           :(
-             <>
-              <EditTitle setEditMode={setEditMode} editTitle={props.editTitle} id={todo.id} title={todo.title} ready={todo.ready}/>
-             </>
-           )
-
+          <EditTitle todo={todo} editData={props.editData} editTitle={props.editTitle}/>
       ))}
     </>
   );
